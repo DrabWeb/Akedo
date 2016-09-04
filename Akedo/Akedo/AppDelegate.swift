a8c0ada3da2098bb9dc9713d8b29c44775950207
DrabWeb <sethunity@gmail.com>
2016-09-04 16:54:07 -0300

    Added base project

---
+++ Akedo/Akedo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Akedo
+//
+//  Created by Seth on 2016-09-04.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
---
+++ Akedo/Akedo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Akedo
+//
+//  Created by Seth on 2016-09-04.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
