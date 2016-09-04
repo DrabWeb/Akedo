dc336e9d4ea0a65c54f07d7a220b7211013be26b
DrabWeb <sethunity@gmail.com>
2016-09-04 17:46:23 -0300

    Added status item

--- Akedo/Akedo/AppDelegate.swift
+++ Akedo/Akedo/AppDelegate.swift
@@ -10,10 +10,25 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    /// The main status item for Akedo
+    var uploadStatusBarItem : NSStatusItem = NSStatusItem();
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        // Create the status bar item
+        createStatusItem();
+    }
+    
+    /// Creates uploadStatusBarItem
+    func createStatusItem() {
+        // Create uploadStatusBarItem
+        uploadStatusBarItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1);
+        
+        // Set the item's image scaling
+        (uploadStatusBarItem.button!.cell as! NSButtonCell).imageScaling = .ScaleProportionallyDown;
+        
+        // Set the icon
+        uploadStatusBarItem.image = NSImage(named: "AKUploadIcon")!;
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
